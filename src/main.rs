5a8dccf8b7bd587062ed2e0615219177530f56fe
Daniel Smith <smith.daniel.br@gmail.com>
2017-03-05 15:19:20 -0500

    Tournament implementation with all teams having equal odds

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,38 @@
 #[macro_use]
 extern crate itertools;
+extern crate threadpool;
+
+use std::collections::HashMap;
+use std::sync::mpsc::channel;
+
+use threadpool::ThreadPool;
 
 mod tournament;
-
 use tournament::Tournament;
 
+fn run_a_bunch_of_tournaments(num_tourneys: u32) -> Vec<Tournament> {
+    let pool = ThreadPool::new(6);
+    let (t_tourneys, r_tourneys) = channel();
+
+    for _ in 0..num_tourneys {
+        let t_tourneys = t_tourneys.clone();
+        pool.execute(move || {
+            let t = tournament::run_tournament();
+            t_tourneys.send(t).unwrap()
+        });
+    }
+
+    drop(t_tourneys);
+    r_tourneys.into_iter().collect()
+}
+
 fn main() {
-    let mut t = Tournament::new();
-    t.run_tournament();
-    println!("{}", t);
+    let tourneys = run_a_bunch_of_tournaments(1000);
+    let mut champion_counts = HashMap::new();
+    for tourney in tourneys {
+        *champion_counts.entry(tourney.champion).or_insert(0) += 1;
+    }
+    for (champ, count) in champion_counts {
+        println!("{} won {} times!", champ, count);
+    }
 }
--- src/tournament.rs
+++ src/tournament.rs
@@ -4,21 +4,68 @@ use std::collections::HashMap;
 use std::fmt;
 
 use itertools::Itertools;
+use self::rand::distributions::{IndependentSample, Range};
 
 pub struct Tournament {
-    rounds_completed: u8,
-    win_probabilities: HashMap<(&'static str, &'static str), f64>,
-    first_round: Vec<&'static str>,
-    second_round: Option<Vec<&'static str>>,
-    sweet_sixteen: Option<Vec<&'static str>>,
-    elite_eight: Option<Vec<&'static str>>,
-    final_four: Option<Vec<&'static str>>,
-    championship: Option<Vec<&'static str>>,
-    champion: Option<&'static str>,
+    pub first_round: Vec<&'static str>,
+    pub second_round: Vec<&'static str>,
+    pub sweet_sixteen: Vec<&'static str>,
+    pub elite_eight: Vec<&'static str>,
+    pub final_four: Vec<&'static str>,
+    pub championship: Vec<&'static str>,
+    pub champion: &'static str,
 }
 
-// The maximum number of rounds a tournament can go.
-const MAX_ROUNDS: u8 = 6;
+pub fn run_tournament() -> Tournament {
+    let mut tournament = Tournament::new();
+    let team_comp = TeamComp::new(&tournament.first_round);
+    let mut rng = rand::thread_rng();
+
+    tournament.second_round = advance_round(&tournament.first_round, &team_comp, &mut rng);
+    tournament.sweet_sixteen = advance_round(&tournament.second_round, &team_comp, &mut rng);
+    tournament.elite_eight = advance_round(&tournament.sweet_sixteen, &team_comp, &mut rng);
+    tournament.final_four = advance_round(&tournament.elite_eight, &team_comp, &mut rng);
+    tournament.championship = advance_round(&tournament.final_four, &team_comp, &mut rng);
+    tournament.champion = advance_round(&tournament.championship, &team_comp, &mut rng)[0];
+
+    tournament
+}
+
+fn advance_round<'a>(round: &Vec<&'a str>,
+                     team_comp: &TeamComp,
+                     mut rng: &mut rand::Rng)
+                     -> Vec<&'a str> {
+    let mut next_round = Vec::new();
+    let between = Range::new(0f64, 1.);
+    for mut chunk in &round.iter().chunks_lazy(2) {
+        let team1 = chunk.next().unwrap();
+        let team2 = chunk.next().unwrap();
+        if team_comp.win_probability(*team1, *team2) > between.ind_sample(&mut rng) {
+            next_round.push(*team1)
+        } else {
+            next_round.push(*team2)
+        }
+    }
+    next_round
+}
+
+pub struct TeamComp {
+    win_probabilities: HashMap<(&'static str, &'static str), f64>,
+}
+
+impl TeamComp {
+    pub fn new(teams: &Vec<&'static str>) -> TeamComp {
+        let mut win_probabilities = HashMap::new();
+        for (team1, team2) in iproduct!(teams, teams) {
+            win_probabilities.insert((*team1, *team2), 0.5);
+        }
+        TeamComp { win_probabilities: win_probabilities }
+    }
+
+    pub fn win_probability(&self, team1: &str, team2: &str) -> f64 {
+        *self.win_probabilities.get(&(team1, team2)).unwrap()
+    }
+}
 
 impl Tournament {
     pub fn new() -> Tournament {
@@ -86,79 +133,16 @@ impl Tournament {
                          "Syracue",
                          "Michigan St",
                          "Middle Tenn"];
-        let mut win_probabilities = HashMap::new();
-        for (team1, team2) in iproduct!(&teams, &teams) {
-            win_probabilities.insert((*team1, *team2), 0.5);
-        }
         Tournament {
-            rounds_completed: 0,
-            win_probabilities: win_probabilities,
             first_round: teams,
-            second_round: None,
-            sweet_sixteen: None,
-            elite_eight: None,
-            final_four: None,
-            championship: None,
-            champion: None,
+            second_round: vec![],
+            sweet_sixteen: vec![],
+            elite_eight: vec![],
+            final_four: vec![],
+            championship: vec![],
+            champion: "",
         }
     }
-
-    pub fn run_tournament(&mut self) {
-        // Don't try to run the full tournament if it's already started.
-        if self.rounds_completed > 0 {
-            return;
-        }
-        while self.rounds_completed < MAX_ROUNDS {
-            self.run_next_round();
-        }
-    }
-
-    fn run_next_round(&mut self) {
-        match self.rounds_completed {
-            0 => {
-                self.second_round = Some(advance_round(&self.first_round, &self.win_probabilities))
-            }
-            1 => {
-                self.sweet_sixteen = Some(advance_round(self.second_round.as_ref().unwrap(),
-                                                        &self.win_probabilities))
-            }
-            2 => {
-                self.elite_eight = Some(advance_round(self.sweet_sixteen.as_ref().unwrap(),
-                                                      &self.win_probabilities))
-            }
-            3 => {
-                self.final_four = Some(advance_round(self.elite_eight.as_ref().unwrap(),
-                                                     &self.win_probabilities))
-            }
-            4 => {
-                self.championship = Some(advance_round(self.final_four.as_ref().unwrap(),
-                                                       &self.win_probabilities))
-            }
-            5 => {
-                self.champion =
-                    Some(advance_round(self.championship.as_ref().unwrap(),
-                                       &self.win_probabilities)[0])
-            }
-            _ => panic!("Tried to advance the round of a completed tournament"),
-        }
-        self.rounds_completed += 1;
-    }
-}
-
-fn advance_round<'a>(round: &Vec<&'a str>,
-                     win_probabilities: &HashMap<(&'a str, &'a str), f64>)
-                     -> Vec<&'a str> {
-    let mut next_round = Vec::new();
-    for mut chunk in &round.iter().chunks_lazy(2) {
-        let team1 = chunk.next().unwrap();
-        let team2 = chunk.next().unwrap();
-        if *win_probabilities.get(&(*team1, *team2)).unwrap() > rand::random() {
-            next_round.push(*team1)
-        } else {
-            next_round.push(*team2)
-        }
-    }
-    next_round
 }
 
 fn print_round(f: &mut fmt::Formatter, round: &Vec<&str>) -> fmt::Result {
@@ -172,29 +156,20 @@ macro_rules! print_round {
     ($f:expr, $round_name:tt, $round_data:expr) => (
         try!(writeln!($f, $round_name));
         try!(writeln!($f, "--------------"));
-        match $round_data {
-            Some(ref round_data) => try!(print_round($f, &round_data)),
-            None => try!(writeln!($f, "Round not started.")),
-        }
+        try!(print_round($f, $round_data));
         try!(writeln!($f, "--------------"));)
 }
 
 impl fmt::Display for Tournament {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         try!(writeln!(f, "--------------"));
-        try!(writeln!(f, "First Round:"));
-        try!(writeln!(f, "--------------"));
-        try!(print_round(f, &self.first_round));
-        try!(writeln!(f, "--------------"));
-        print_round!(f, "Second Round:", self.second_round);
-        print_round!(f, "Sweet Sixteen:", self.sweet_sixteen);
-        print_round!(f, "Elite Eight:", self.elite_eight);
-        print_round!(f, "Final Four:", self.final_four);
-        print_round!(f, "Championship:", self.championship);
-        match self.champion {
-            Some(ref champion) => try!(writeln!(f, "Your champion is: {}.", champion)),
-            None => try!(writeln!(f, "No champion crowned yet.")),
-        }
+        print_round!(f, "First Round:", &self.first_round);
+        print_round!(f, "Second Round:", &self.second_round);
+        print_round!(f, "Sweet Sixteen:", &self.sweet_sixteen);
+        print_round!(f, "Elite Eight:", &self.elite_eight);
+        print_round!(f, "Final Four:", &self.final_four);
+        print_round!(f, "Championship:", &self.championship);
+        try!(writeln!(f, "Your champion is: {}.", &self.champion));
         Ok(())
     }
 }
